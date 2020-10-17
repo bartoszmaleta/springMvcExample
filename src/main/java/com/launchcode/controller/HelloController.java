a9defe49cfaff6c26c843edf7b5a09c2f1ef0a77
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-10-17 21:27:15 +0200

    Add hello with parameter from URL

--- src/main/java/com/launchcode/controller/HelloController.java
+++ src/main/java/com/launchcode/controller/HelloController.java
@@ -6,22 +6,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class HelloController {
 
     // localhost:8080/hello
-
-
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     @ResponseBody
     public String hello() {
         return "<h1>Hello, world!</h1>";
     }
 
+    // localhost:8080/helloWithName?name=Bartosz
     @RequestMapping(value = "/helloWithName", method = RequestMethod.GET)
     @ResponseBody
-    public String helloWithName() {
-        return String.format("<h1>%s</h1>", HelloMessage.getMessage("Bartosz"));
+    public String helloWithName(HttpServletRequest httpServletRequest) {
+        // get name parameter from request
+        String name = httpServletRequest.getParameter("name");
+
+
+        return String.format("<h1>%s</h1>", HelloMessage.getMessage(name));
     }
 
 }
